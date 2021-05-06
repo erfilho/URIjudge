fc4adf9df59a24909faeb9abcbccfc574c7c6962
erfilho <erineldot@gmail.com>
2021-05-06 12:01:14 -0300

    "01 - Created archives 1001 - 1037 for URIjudge challenges"

---
+++ Iniciante/1001.py
@@ -0,0 +1,6 @@
+num = int(input())
+num2 = int(input())
+
+x = num + num2
+
+print(f'X = {x}')
---
+++ Iniciante/1002.py
@@ -0,0 +1,8 @@
+from math import pow
+
+raio = float(input())
+pi = 3.14159
+
+area = pow(raio, 2)*pi
+
+print(f'A={area:.4f}')
